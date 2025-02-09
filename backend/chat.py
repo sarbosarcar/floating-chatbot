0040da8dbd2180e17bdd13fbc914fdc024367292
sarbosarcar <sarbosarcar@gmail.com>
2025-02-10 00:03:12 +0530

    Update files

--- backend/chat.py
+++ backend/chat.py
@@ -11,33 +11,37 @@ import json
 dotenv.load_dotenv()
 API_KEY = os.environ.get("HUGGINGFACEHUB_API_KEY")
 
-# model_id = "meta-llama/Meta-Llama-3-8B-Instruct" 
 model_id = "meta-llama/Meta-Llama-3-8B-Instruct" 
+# model_id = "deepset/roberta-base-squad2" 
 
 # Initialize the Hugging Face inference client
 client = InferenceClient(model=model_id, token=API_KEY)
 
 # Define the prompt template for a chatbot system
 PROMPT_TEMPLATE = """
+You are an AI chatbot integrated on the Srijan website organized by the Faculty of Engineering & Technology Students' Union (F.E.T.S.U.) at Jadavpur University.Your task is to help users with their queries by providing appropriate responses based on the context provided. If you do not know an answer clearly state so. You will not add extra information, do not assume anything and provide the clearest possible answer. If the spellings for events retrieved do not match, do not reply with any details.
+```
 Context:
 {context}
-
+```
+```
 User Query:
 {query}
-
+```
 Generate a helpful response.
+Answer:
 """
 
 # Function to generate a chatbot response
 def prompt_answer(query, context):
     # Construct the prompt
+    prompt = PROMPT_TEMPLATE.format(context=context, query=query)
     messages = [
         {
             "role": "system",
-            "content": "You are an AI chatbot integrated on a website, helping users with their queries by providing appropriate responses based on the context provided. Respond in a concise, informative manner and maintain an approachable tone. If you do not know an answer clearly state so. You will not add extra information and provide the shortest, clearest possible answer. If the spellings for events retrieved do not match, do not reply with any details."
+            "content": "You are an AI chatbot integrated on a website, helping users with their queries by providing appropriate responses based on the context provided. If you do not know an answer clearly state so. You will not add extra information, do not assume anything and provide the clearest possible answer. If the spellings for events retrieved do not match, do not reply with any details."
         },
     ]
-    prompt = PROMPT_TEMPLATE.format(context=context, query=query)
     messages.append(
         {
             "role": "user",
@@ -46,9 +50,11 @@ def prompt_answer(query, context):
     )
     try:
         # Call the inference client
-        response = client.chat_completion(messages, max_tokens=2048).choices[0].message.content
+        # response = client.chat_completion(messages, max_new_tokens=128).choices[0].message.content
+        response = client.text_generation(prompt, max_new_tokens=128, temperature=0.4).strip()
     except Exception as e:
         response = f"It seems like there was some trouble responding to the query! Please retry."
+        print(e)
     return response
 
 def get_response(question: str):
